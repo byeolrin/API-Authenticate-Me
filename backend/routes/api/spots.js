60d1712caefb9b401203c8947b295c88ee98b414
Gary Cheung <garycheung78@gmail.com>
2024-01-03 13:21:50 -0500

    spacing issues

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,9 +6,7 @@ const { requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-
 const router = express.Router();
-
 
 const validateSpots = [
     check('address')
