5a55af88458dbaa09303953ad6a9286cfe155121
Gary Cheung <garycheung78@gmail.com>
2024-01-07 00:11:27 -0500

    fixed seeder, migrations, model, date format for bookings

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -27,11 +27,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
     },
     startDate: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       allowNull: false
     },
     endDate: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       allowNull: false
     },
   }, {
