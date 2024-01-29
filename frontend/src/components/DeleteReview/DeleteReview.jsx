52d52a8dbc1fc895980b772fe970a6a3d328e4c7
Gary Cheung <garycheung78@gmail.com>
2024-01-29 02:20:09 -0500

    final styling for project submition

--- frontend/src/components/DeleteReview/DeleteReview.jsx
+++ frontend/src/components/DeleteReview/DeleteReview.jsx
@@ -1,27 +1,32 @@
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { thunkDeleteReview } from "../../store/review";
+import "./DeleteReview.css";
 
 const DeleteReview = ({ reviewId }) => {
-    const dispatch = useDispatch();
-    const { closeModal } = useModal();
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
-    const deleteHandler = (e) => {
-        e.preventDefault();
-        dispatch(thunkDeleteReview(reviewId));
-        closeModal();
-    }
+  const deleteHandler = (e) => {
+    e.preventDefault();
+    dispatch(thunkDeleteReview(reviewId));
+    closeModal();
+  };
 
-    return (
-        <div className="delete-check-container">
-            <h1>Confirm Delete</h1>
-            <div className="delete-button-container">
-                <p>Are you sure you want to remove this review?</p>
-                <button id='confirm-delete'className='delete-buttons' onClick={deleteHandler}>Yes (Delete Review)</button>
-                <button id='no-delete' className="delete-buttons" onClick={closeModal}>No (Keep Review)</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="delete-review-container">
+      <h1>Confirm Delete</h1>
+      <p>Are you sure you want to remove this review?</p>
+      <div className="delete-button-container">
+        <button id="confirm-delete" onClick={deleteHandler}>
+          Yes (Delete Review)
+        </button>
+        <button id="no-delete" onClick={closeModal}>
+          No (Keep Review)
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DeleteReview;
